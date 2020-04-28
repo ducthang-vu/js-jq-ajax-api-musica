4e0eea260d50e7f5316e8030a4e8a50e159e2eea
Duc Thang Vu <vuducthang91@gmail.com>
2020-04-28 17:19:19 +0200

    bonus completed

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
 
-	function printCard(container, object, template) {
-		var source = template.html();
-		var template = Handlebars.compile(source);
+	var source = $('#entry-template').html();
+	var template = Handlebars.compile(source);
+
+	function printCard(container, object) {
 		var context = {
 			poster: object.poster,
 			title: object.title,
@@ -12,15 +13,31 @@ $(document).ready(function() {
 		
 		container.append(template(context));
 	}
-	
+
 
 	$.ajax({
 		url: "https://flynn.boolean.careers/exercises/api/array/music",
 		method: "GET",
 
 		success: data => {
+			// print all on page load
 			data.response.forEach(item =>
-				printCard($('.cds-container'), item, $('#entry-template')))
+				printCard($('.cds-container'), item)
+			)
+
+			// print when select input is changed
+			$('#genre-select').change(function() {
+				$('.cds-container').html('')
+				var userChoise = $(this).children(':selected')[0].value;
+				
+				data.response.forEach(item => {
+					if (userChoise === 'all') {printCard($('.cds-container'), item)}
+					else if (item.genre === userChoise) {
+						printCard($('.cds-container'), item)
+					}
+				});
+					
+			})
 		},
 
 		error: () => console.log('Error')
